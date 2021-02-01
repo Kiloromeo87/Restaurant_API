1c041804f58201cfe09645ceb82e5bd1bfc67c3a
Daniel Varsas <daniel.varsas@gmail.com>
2021-02-01 22:44:31 +0100

    2. commit

--- src/main/java/RestaurantAPI/demo/RestaurantApiApplication.java
+++ src/main/java/RestaurantAPI/demo/RestaurantApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestaurantApiApplication {
-
+//something
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApiApplication.class, args);
 	}
